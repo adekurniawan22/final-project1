769911de658cf9a36d415a9b14352b36aa6222fb
Ade Kurniawan <ade.kurniawan216@gmail.com>
2022-10-14 15:01:14 +0700

    new

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const User = require('./models/user');
+// const User = require('./models/user');
 const app = express();
 const port = 4000;
 
@@ -7,7 +7,7 @@ app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.get('/api/v1', (req, res) => { res.render('index') })
-app.post('/api/v1/users/register', User.register);
+// app.post('/api/v1/users/register', User.register);
 app.post('/api/v1/users/login');
 
 app.listen(port, () => {
