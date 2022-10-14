b228cecb2f72073275a4d4674a6afa1921ba4004
Ade Kurniawan <ade.kurniawan216@gmail.com>
2022-10-15 04:06:17 +0700

    fix program code

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -1,25 +1,20 @@
 const { verifyToken } = require('../helper/jwt');
 const UserModels = require('../models/queries');
 
-async function authentiction(req, res, next) {
-
+async function authentication(req, res, next) {
     try {
         const token = req.headers.authorization;
         const userDecoded = verifyToken(token);
-        const user = await UserModels.postgres.query(`SELECT * FROM users WHERE id= ${userDecoded.id}`);
-
+        const user = await UserModels.postgres.query(`SELECT * FROM users WHERE id=${+userDecoded.id}`);
 
         if (!user) {
-            return res.status(500).json({ message: 'You can acces this endpoint' });
+            return res.status(500).json({ message: 'You must login first' });
         }
         res.dataUser = user.rows[0];
         next();
     } catch (error) {
-        const token = req.headers.Authorization;
-        console.log(token);
-        return res.status(500).json(error);
-
+        return res.status(500).json({ message: 'hahaha' });
     }
 }
 
-module.exports = authentiction;
+module.exports = authentication;
--- models/queries.js
+++ models/queries.js
@@ -9,40 +9,61 @@ const postgres = new Postgres({
 })
 
 const register = (req, res) => {
-    const { email, password } = req.body;
-    postgres.query(`INSERT INTO users (email,password) VALUES ($1,$2)`, [email, password], (error, result) => {
-        if (error) {
-            return res.status(500).json({ message: 'Something wrong' })
+    try {
+        const { email, password } = req.body;
+        //checking value email and password
+        if (email === "" || password === "") {
+            return res.status(500).json({ message: 'Email and password cannot be empty' })
         }
+        //query
+        postgres.query(`INSERT INTO users (email,password) VALUES ($1,$2)`, [email, password]);
         return res.status(201).json({ message: 'Success create new user' })
-    })
+    } catch (error) {
+        return res.status(500).json(error)
+    }
 }
 
 const login = async (req, res) => {
-    const { email, password } = req.body;
-    const query = await postgres.query(`SELECT * FROM users WHERE email = '${email}'`);
-    dataLogin = query.rows[0];
-
-
-    if (query.rowCount > 0) {
-        if (password == query.rows[0].password) {
-            const token = generateToken({ id: dataLogin.id });
-            return res.status(200).json({ token: token });
+    try {
+        const { email, password } = req.body;
+        //checking value email and password
+        if (email === "" || password === "") {
+            return res.status(500).json({ message: 'Email and password cannot be empty' })
         }
-    } else {
-        return res.status(500).json({ message: 'User Not Found' })
+        const query = await postgres.query(`SELECT * FROM users WHERE email = '${email}'`);
+        const dataLogin = query.rows[0];
+
+        if (query.rowCount > 0) {
+            if (password == query.rows[0].password) {
+                const token = generateToken({ id: dataLogin.id });
+                return res.status(200).json({ token: token });
+            } else {
+                return res.status(500).json({ message: 'Wrong password' })
+            }
+        } else {
+            return res.status(500).json({ message: 'User Not Found' })
+        }
+    } catch (error) {
+        return res.status(500).json(error)
     }
 }
 
 const createReflections = async (req, res) => {
-    const authenticatedUserId = res.dataUser.id;
-    const { success, low_point, take_away, owner_id, created_date, modified_date } = req.body;
-    postgres.query(`INSERT INTO reflections (success, low_point, take_away, owner_id, created_date, modified_date) VALUES ($1,$2,$3,$4,$5,$6)`, [success, low_point, take_away, owner_id, created_date, modified_date], () => {
+    try {
+        const authenticatedUserId = res.dataUser.id;
+        const { success, low_point, take_away, owner_id, created_date, modified_date } = req.body;
+        console.log(authenticatedUserId);
+        console.log(success, low_point, take_away, owner_id, created_date, modified_date)
+        await postgres.query(`INSERT INTO reflections (success, low_point, take_away, owner_id, created_date, modified_date) VALUES ($1,$2,$3,$4,$5,$6)`, [success, low_point, take_away, owner_id, created_date, modified_date]);
+
         if (owner_id != authenticatedUserId) {
-            return res.status(500).json({ message: 'You only can create reflection with your id' })
+            return res.status(500).json({ message: 'You only can create reflection with your id' });
         }
-        return res.status(201).json({ message: 'Success create new reflections' })
-    })
+        return res.status(201).json({ message: 'Success create new reflections' });
+    } catch (error) {
+        return res.status(201).json(error);
+    }
+
 }
 
 const getData = async (req, res) => {
@@ -52,6 +73,7 @@ const getData = async (req, res) => {
 }
 
 module.exports = {
+    postgres,
     register,
     login,
     getData,
